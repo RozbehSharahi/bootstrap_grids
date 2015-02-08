5a08c561ee184462cd74f05617181afd00f0ccd8
Pascal Mayer <laxap4@gmail.com>
2015-02-08 12:43:41 +0100

    use of extension conf options

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,4 +7,15 @@ if ( strlen($_EXTCONF) ) {
 	$extConf = unserialize($_EXTCONF);
 }
 
+// Define grids by pageTS config
+// Special thank goes to Daniel Corn.
+// Only if not set to use old record based uids
+if ( ! isset($extConf['useOldRecordIds']) || ! $extConf['useOldRecordIds'] ) {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bootstrap_grids/Configuration/TypoScript/tsconfig.ts">');
+
+	// Only if enabled
+	if ( isset($extConf['enableGridSimpleRow']) && $extConf['enableGridSimpleRow'] ) {
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bootstrap_grids/Configuration/TypoScript/simpleRow/tsconfig.ts">');
+	}
+}
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -7,12 +7,13 @@ if ( strlen($_EXTCONF) ) {
 	$extConf = unserialize($_EXTCONF);
 }
 
-// --------------------------------------------------------------------
-// Default bootstrap_grids settings
-// --------------------------------------------------------------------
-//
 // Add static typoscript configurations
+// Default stuff and grid definitions
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Grids for Bootstrap');
 
+// Grids based on db record uid vs. new version with names as layout ids
+if ( isset($extConf['useOldRecordIds']) && $extConf['useOldRecordIds'] ) {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/old', 'Grids for db records (uids)');
+}
 
 ?>
