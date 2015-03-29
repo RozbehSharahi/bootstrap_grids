d0cc0057f2600a7cb4953270a615fe165d70ac2c
Pascal Mayer <laxap4@gmail.com>
2015-03-29 19:20:18 +0200

    Dependency updated for TYPO3 7.1.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => 'typo3@bsdist.ch',
 	'author_company' => '',
 	'shy' => '',
-	'version' => '1.1.0',
+	'version' => '1.1.1',
 	'priority' => '',
 	'module' => '',
 	'state' => 'beta',
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'lockType' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.1.99',
 			'gridelements' => '3.0.0-0.0.0',
 		),
 		'conflicts' => array(
