75901415adfb189b28f0b77cdcadceccd2060220
Pascal Mayer <laxap4@gmail.com>
2016-02-08 22:40:17 +0100

    handling of useOldRecordIds and enableGridSimpleRow removed

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,21 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
-// --- Get extension configuration ---
-$extConf = array();
-if ( strlen($_EXTCONF) ) {
-	$extConf = unserialize($_EXTCONF);
-}
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bootstrap_grids/Configuration/TypoScript/tsconfig.ts">');
 
-// Define grids by pageTS config
-// Special thank goes to Daniel Corn.
-// Only if not set to use old record based uids
-if ( ! isset($extConf['useOldRecordIds']) || ! $extConf['useOldRecordIds'] ) {
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bootstrap_grids/Configuration/TypoScript/tsconfig.ts">');
-
-	// Only if enabled
-	if ( isset($extConf['enableGridSimpleRow']) && $extConf['enableGridSimpleRow'] ) {
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bootstrap_grids/Configuration/TypoScript/simpleRow/tsconfig.ts">');
-	}
-}
 ?>
