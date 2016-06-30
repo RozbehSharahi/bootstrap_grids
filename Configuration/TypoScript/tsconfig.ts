0f26ba6be8144d3b8e74e259f153978e1c32e6d7
Michael Grundkötter <mg@ujamii.com>
2016-06-30 11:54:11 +0200

    - added new type: carousel, implementing the default http://getbootstrap.com/javascript/#carousel

--- Configuration/TypoScript/tsconfig.ts
+++ Configuration/TypoScript/tsconfig.ts
@@ -223,5 +223,22 @@ tx_gridelements {
             }
             flexformDS = FILE:EXT:bootstrap_grids/Configuration/FlexForm/flexform_slider.xml
         }
+
+        carousel {
+            title = LLL:EXT:bootstrap_grids/Resources/Private/Language/locallang_db.xlf:carousel.title
+            description = LLL:EXT:bootstrap_grids/Resources/Private/Language/locallang_db.xlf:carousel.description
+            icon = EXT:bootstrap_grids/Resources/Public/Icons/grid_slider.png
+            frame = 1
+            topLevelLayout = 0
+            config {
+                colCount = 1
+                rowCount = 1
+                rows.1.columns.1 {
+                    name = LLL:EXT:bootstrap_grids/Resources/Private/Language/locallang_db.xlf:celayout.carouselElements
+                    colPos = 101
+                }
+            }
+            flexformDS = FILE:EXT:bootstrap_grids/Configuration/FlexForm/flexform_carousel.xml
+        }
     }
 }
