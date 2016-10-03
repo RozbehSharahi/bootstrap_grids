08b4c6fe92f9792f154ede4ecb01ecd0ed436179
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2016-10-03 16:45:33 +0200

    [FEATURE] Add 1 column
    Releases: master

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,5 +2,3 @@
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bootstrap_grids/Configuration/TypoScript/tsconfig.ts">');
-
-?>
