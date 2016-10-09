169a6d4c8b10c2d87fbf04f86a937b1ef14fe8de
Pascal Mayer <laxap4@gmail.com>
2016-10-09 20:00:16 +0200

    support optional simpleRow element

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,16 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
+// --- Get extension configuration ---
+$extConf = array();
+if ( strlen($_EXTCONF) ) {
+	$extConf = unserialize($_EXTCONF);
+}
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bootstrap_grids/Configuration/TypoScript/tsconfig.ts">');
 
+// Only if enabled
+if ( isset($extConf['enableGridSimpleRow']) && $extConf['enableGridSimpleRow'] ) {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bootstrap_grids/Configuration/TypoScript/simpleRow/tsconfig.ts">');
+}
 ?>
