bf40b5195c0a470f624d2d35329e5817e1c1dea7
Pascal Mayer <typo3@bsdist.ch>
2017-04-08 22:17:10 +0200

    Release 1.4.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,32 +4,30 @@
  * Extension Manager/Repository config file for ext: "bootstrap_grids"
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'Grids for bootstrap',
 	'description' => 'Gridelements for bootstrap. Column grids, grids for simple accordions, tabs and content slider.',
 	'category' => 'misc',
 	'author' => 'Pascal Mayer',
 	'author_email' => 'typo3@bsdist.ch',
 	'author_company' => '',
-	'shy' => '',
-	'version' => '1.3.1',
-	'priority' => '',
-	'module' => '',
+	'version' => '1.4.0',
 	'state' => 'stable',
-	'internal' => '',
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
-	'lockType' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.6.0-7.6.99',
-			'gridelements' => '7.0.0-0.0.0',
-		),
-		'conflicts' => array(
-		),
-	),
-);
+	'constraints' => [
+		'depends' => [
+			'typo3' => '8.7.0-8.7.99',
+			'gridelements' => '8.0.0-0.0.0',
+		],
+		'conflicts' => [
+		],
+	],
+    'autoload' => [
+        'psr-4' => ['Laxap\\BootstrapGrids\\' => 'Classes']
+    ],
+];
 
 ?>
