9a1b14b6749bdd6bc00ab16bcdbf24aaa1efc3ef
Pascal Mayer <typo3@bsdist.ch>
2017-04-08 22:16:00 +0200

    code moved from ext_tables.php

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,7 @@
+<?php
+// Add an entry in the static template list found in sys_templates for static TS
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'bootstrap_grids',
+    'Configuration/TypoScript',
+    'Grids for Bootstrap'
+);
