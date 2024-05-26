a1eb91cd4716f03f88d8087530db572bf100846e
Rozbeh Chiryai Sharahi <rozbeh.sharahi@gmail.com>
2024-05-26 18:07:31 +0100

    More modern config for config scripts

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,12 +1,16 @@
 <?php
 // Add an entry in the static template list found in sys_templates for static TS
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'bootstrap_grids',
-    'Configuration/TypoScript/',
-    'Grids for Bootstrap (deprecated)'
-);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'bootstrap_grids',
-    'Configuration/TypoScript/DataProcessingLibContentElement/',
-    'Grids for Bootstrap (recommended, w/DataProcessing)'
-);
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+(static function() {
+    ExtensionManagementUtility::addStaticFile(
+        'bootstrap_grids',
+        'Configuration/TypoScript/',
+        'Grids for Bootstrap (deprecated)'
+    );
+    ExtensionManagementUtility::addStaticFile(
+        'bootstrap_grids',
+        'Configuration/TypoScript/DataProcessingLibContentElement/',
+        'Grids for Bootstrap (recommended, w/DataProcessing)'
+    );
+})();
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,50 +1,55 @@
 <?php
-if (!defined('TYPO3_MODE')) { die('Access denied.'); }
 
-call_user_func(
-    function () {
+use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
+use TYPO3\CMS\Core\Imaging\IconRegistry;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-        // register icons
-        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-        $iconRegistry->registerIcon(
-            'tx-bootstrapgrids-col2',
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col2.gif']
-        );
-        $iconRegistry->registerIcon(
-            'tx-bootstrapgrids-col3',
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col3.gif']
-        );
-        $iconRegistry->registerIcon(
-            'tx-bootstrapgrids-col4',
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col4.gif']
-        );
-        $iconRegistry->registerIcon(
-            'tx-bootstrapgrids-simpletabs',
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_simpletabs.png']
-        );
-        $iconRegistry->registerIcon(
-            'tx-bootstrapgrids-tabs4',
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_tabs4.png']
-        );
-        $iconRegistry->registerIcon(
-            'tx-bootstrapgrids-tabs6',
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_tabs6.png']
-        );
-        $iconRegistry->registerIcon(
-            'tx-bootstrapgrids-accordion',
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_accordion.png']
-        );
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
 
-        // Add pageTS config
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bootstrap_grids/Configuration/TypoScript/pageTs/tsconfig.ts">');
+(static function () {
 
-    }
-);
-?>
+    // register icons
+    $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'tx-bootstrapgrids-col2',
+        BitmapIconProvider::class,
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col2.gif']
+    );
+    $iconRegistry->registerIcon(
+        'tx-bootstrapgrids-col3',
+        BitmapIconProvider::class,
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col3.gif']
+    );
+    $iconRegistry->registerIcon(
+        'tx-bootstrapgrids-col4',
+        BitmapIconProvider::class,
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col4.gif']
+    );
+    $iconRegistry->registerIcon(
+        'tx-bootstrapgrids-simpletabs',
+        BitmapIconProvider::class,
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_simpletabs.png']
+    );
+    $iconRegistry->registerIcon(
+        'tx-bootstrapgrids-tabs4',
+        BitmapIconProvider::class,
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_tabs4.png']
+    );
+    $iconRegistry->registerIcon(
+        'tx-bootstrapgrids-tabs6',
+        BitmapIconProvider::class,
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_tabs6.png']
+    );
+    $iconRegistry->registerIcon(
+        'tx-bootstrapgrids-accordion',
+        BitmapIconProvider::class,
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_accordion.png']
+    );
+
+    // Add pageTS config
+    ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bootstrap_grids/Configuration/TypoScript/pageTs/tsconfig.ts">');
+
+})();
