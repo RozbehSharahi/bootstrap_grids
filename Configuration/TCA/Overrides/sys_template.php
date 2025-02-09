fe74c9a1ce7c460fbcf174de12f3d100c2fe2e09
Rozbeh Chiryai Sharahi <rozbeh.sharahi@gmail.com>
2025-02-09 19:47:24 +0000

    Update templates for TYPO3 v12 compatibility
    Removed outdated templates and adjusted structure to ensure compatibility with TYPO3 version 12.

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,16 +1,10 @@
 <?php
-// Add an entry in the static template list found in sys_templates for static TS
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 (static function() {
     ExtensionManagementUtility::addStaticFile(
         'bootstrap_grids',
-        'Configuration/TypoScript/',
-        'Grids for Bootstrap (deprecated)'
-    );
-    ExtensionManagementUtility::addStaticFile(
-        'bootstrap_grids',
-        'Configuration/TypoScript/DataProcessingLibContentElement/',
-        'Grids for Bootstrap (recommended, w/DataProcessing)'
+        'Configuration/TypoScript/Frontend/',
+        'Grids for Bootstrap'
     );
 })();
