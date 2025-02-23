83ca18cd9c60a8b948123dd3e0ed912ed5ac6ebe
Charles Coleman <charles@rapidfyre.com>
2025-02-23 08:56:02 -0800

    Add override for password that pulls from PASSWORD env var

---
+++ .docker/templates/v11/config/system/additional.php
@@ -0,0 +1,2 @@
+<?php
+$GLOBALS['TYPO3_CONF_VARS']['BE']['installToolPassword'] = getenv('PASSWORD') ?: '$argon2i$v=19$m=65536,t=16,p=1$ZTdDc09PajFITXVoZ3R5eA$FpRJciOI8TyfD4jaKWUmpJkoyUQB16oaUGVjOwU805U';
