3bc4e42f568e62e84be7cfa2e45cd9ef2b567a8c
Charles Coleman <charles@rapidfyre.com>
2025-02-26 02:18:26 -0700

    Migrate to new custom svg icons

--- ext_localconf.php
+++ ext_localconf.php
@@ -14,37 +14,37 @@ defined('TYPO3') or die();
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-col2',
         BitmapIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col2.gif']
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col2.svg']
     );
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-col3',
         BitmapIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col3.gif']
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col3.svg']
     );
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-col4',
         BitmapIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col4.gif']
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col4.svg']
     );
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-simpletabs',
         BitmapIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_simpletabs.png']
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_simpletabs.svg']
     );
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-tabs4',
         BitmapIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_tabs4.png']
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_tabs4.svg']
     );
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-tabs6',
         BitmapIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_tabs6.png']
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_tabs6.svg']
     );
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-accordion',
         BitmapIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_accordion.png']
+        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_accordion.svg']
     );
 
     // Add pageTS config
