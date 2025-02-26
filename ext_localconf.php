505ec1d081a9e216a5691806ad65ac8b236a965d
Charles Coleman <charles@rapidfyre.com>
2025-02-26 02:28:58 -0700

    Use SvgIconProvider instead of BitmapIconProvider in ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 use TYPO3\CMS\Core\Imaging\IconRegistry;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -13,37 +13,37 @@ defined('TYPO3') or die();
     $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-col2',
-        BitmapIconProvider::class,
+        SvgIconProvider::class,
         ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col2.svg']
     );
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-col3',
-        BitmapIconProvider::class,
+        SvgIconProvider::class,
         ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col3.svg']
     );
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-col4',
-        BitmapIconProvider::class,
+        SvgIconProvider::class,
         ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col4.svg']
     );
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-simpletabs',
-        BitmapIconProvider::class,
+        SvgIconProvider::class,
         ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_simpletabs.svg']
     );
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-tabs4',
-        BitmapIconProvider::class,
+        SvgIconProvider::class,
         ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_tabs4.svg']
     );
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-tabs6',
-        BitmapIconProvider::class,
+        SvgIconProvider::class,
         ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_tabs6.svg']
     );
     $iconRegistry->registerIcon(
         'tx-bootstrapgrids-accordion',
-        BitmapIconProvider::class,
+        SvgIconProvider::class,
         ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_accordion.svg']
     );
 
