055bcc2fa3e1271e45d00f22dd5d3700f31ccad3
Charles Coleman <charles@rapidfyre.com>
2025-02-26 14:19:33 -0700

    Create a foreach in Icons.php and moved TypoScript out of Icons.php to setup.typoscript and page.tsconfig

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,60 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
-use TYPO3\CMS\Core\Imaging\IconRegistry;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-defined('TYPO3') or die();
+// Register icons
+$iconList = [];
+foreach ([
+    'tx-bootstrapgrids-col2'       => 'gridlayout_col2.svg',
+    'tx-bootstrapgrids-col3'       => 'gridlayout_col3.svg',
+    'tx-bootstrapgrids-col4'       => 'gridlayout_col4.svg',
+    'tx-bootstrapgrids-simpletabs' => 'grid_simpletabs.svg',
+    'tx-bootstrapgrids-tabs4'      => 'grid_tabs4.svg',
+    'tx-bootstrapgrids-tabs6'      => 'grid_tabs6.svg',
+    'tx-bootstrapgrids-accordion'  => 'grid_accordion.svg',
+] as $identifier => $path) {
+    $iconList[$identifier] = [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:bootstrap_grids/Resources/Public/Icons/' . $path,
+    ];
+}
 
-(static function () {
-
-    // register icons
-    $iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
-    $iconRegistry->registerIcon(
-        'tx-bootstrapgrids-col2',
-        SvgIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col2.svg']
-    );
-    $iconRegistry->registerIcon(
-        'tx-bootstrapgrids-col3',
-        SvgIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col3.svg']
-    );
-    $iconRegistry->registerIcon(
-        'tx-bootstrapgrids-col4',
-        SvgIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/gridlayout_col4.svg']
-    );
-    $iconRegistry->registerIcon(
-        'tx-bootstrapgrids-simpletabs',
-        SvgIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_simpletabs.svg']
-    );
-    $iconRegistry->registerIcon(
-        'tx-bootstrapgrids-tabs4',
-        SvgIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_tabs4.svg']
-    );
-    $iconRegistry->registerIcon(
-        'tx-bootstrapgrids-tabs6',
-        SvgIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_tabs6.svg']
-    );
-    $iconRegistry->registerIcon(
-        'tx-bootstrapgrids-accordion',
-        SvgIconProvider::class,
-        ['source' => 'EXT:bootstrap_grids/Resources/Public/Icons/grid_accordion.svg']
-    );
-
-    // Add pageTS config
-    ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:bootstrap_grids/Configuration/TypoScript/Backend/setup.typoscript">');
-
-    if(getenv('TYPO3_CONTEXT') === 'Development/BootstrapGrids/Docker') {
-        ExtensionManagementUtility::addTypoScript(
-            'bootstrap_grids',
-            'setup',
-            "@import 'EXT:bootstrap_grids/Configuration/TypoScript/Contribution/setup.typoscript'",
-        );
-    }
-})();
+return $iconList;
