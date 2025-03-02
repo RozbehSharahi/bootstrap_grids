b8bc115c586dc0f6c14e27c8f502a6f6d2c4ed23
Rozbeh Chiryai Sharahi <rozbeh.sharahi@gmail.com>
2025-03-02 16:35:33 +0000

    "Refactor TypoScript, add lowlevel dependency, and code-style in Icons.php
    Simplified TypoScript configuration, added ext_localconf.php for Backend setup, and replaced page.tsconfig file. Updated composer.json to include the TYPO3 lowlevel package.

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -4,17 +4,19 @@ declare(strict_types=1);
 
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
-// Register icons
 $iconList = [];
-foreach ([
-    'tx-bootstrapgrids-col2'       => 'gridlayout_col2.svg',
-    'tx-bootstrapgrids-col3'       => 'gridlayout_col3.svg',
-    'tx-bootstrapgrids-col4'       => 'gridlayout_col4.svg',
+
+$iconsMap = [
+    'tx-bootstrapgrids-col2' => 'gridlayout_col2.svg',
+    'tx-bootstrapgrids-col3' => 'gridlayout_col3.svg',
+    'tx-bootstrapgrids-col4' => 'gridlayout_col4.svg',
     'tx-bootstrapgrids-simpletabs' => 'grid_simpletabs.svg',
-    'tx-bootstrapgrids-tabs4'      => 'grid_tabs4.svg',
-    'tx-bootstrapgrids-tabs6'      => 'grid_tabs6.svg',
-    'tx-bootstrapgrids-accordion'  => 'grid_accordion.svg',
-] as $identifier => $path) {
+    'tx-bootstrapgrids-tabs4' => 'grid_tabs4.svg',
+    'tx-bootstrapgrids-tabs6' => 'grid_tabs6.svg',
+    'tx-bootstrapgrids-accordion' => 'grid_accordion.svg',
+];
+
+foreach ($iconsMap as $identifier => $path) {
     $iconList[$identifier] = [
         'provider' => SvgIconProvider::class,
         'source' => 'EXT:bootstrap_grids/Resources/Public/Icons/' . $path,
