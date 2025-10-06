0a3671c21740f8d35577a057adfbd38349129d18
Dennis Robota <dennis.robota@kandoh.de>
2025-10-06 09:44:25 +0200

    [TASK] Set version on 13.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,12 +8,12 @@ $EM_CONF[$_EXTKEY] = [
 	'author' => 'Rozbeh Chiryai Sharahi',
 	'author_email' => 'rozbeh.sharahi+bootstrap_grids@gmail.com',
 	'author_company' => '',
-	'version' => '5.0.0',
+	'version' => '13.0.0',
 	'state' => 'stable',
 	'constraints' => [
 		'depends' => [
 			'typo3' => '13.4.0-13.4.99',
-			'gridelements' => '12.0.0-12.99.99',
+			'gridelements' => '13.0.0-13.99.99',
 		],
 		'conflicts' => [],
         'suggests' => [],
