9a395a06461fb872937018114ad2d4d624534578
Rozbeh Chiryai Sharahi <rozbeh.sharahi@gmail.com>
2025-10-17 18:30:03 +0100

    Remove usage of deprecated ExtensionManagementUtility::addPageTSConfig()

--- ext_localconf.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
-(static function() {
-    ExtensionManagementUtility::addPageTSConfig('
-        @import "EXT:bootstrap_grids/Configuration/TypoScript/Backend/setup.tsconfig"
-    ');
-})();
