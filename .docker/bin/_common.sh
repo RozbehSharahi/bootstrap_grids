deedd2707754e00b8babb2c7b7dfdf294c519501
Charles Coleman <charles@rapidfyre.com>
2026-08-12 14:21:45 -0600

    Use one Composer platform-ignore path via container env.
    Pass COMPOSER_IGNORE_PLATFORM_REQ into the app service and expand --ignore-platform-req there for both startup install and the composer helper. Removes the extra host-side array plumbing.

--- .docker/bin/_common.sh
+++ .docker/bin/_common.sh
@@ -15,11 +15,3 @@ load_docker_env() {
     source .docker/.env
     set +a
 }
-
-set_composer_ignore_platform_arg() {
-    # If env var COMPOSER_IGNORE_PLATFORM_REQ is set, pass --ignore-platform-req to Composer with value of COMPOSER_IGNORE_PLATFORM_REQ.
-    COMPOSER_IGNORE_PLATFORM_ARG=()
-    if [[ -n "${COMPOSER_IGNORE_PLATFORM_REQ:-}" ]]; then
-        COMPOSER_IGNORE_PLATFORM_ARG=(--ignore-platform-req="${COMPOSER_IGNORE_PLATFORM_REQ}")
-    fi
-}
