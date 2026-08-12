4b3c73ff69dd5c135665b6483ddd65a999758686
Charles Coleman <charles@rapidfyre.com>
2026-08-12 15:16:25 -0600

    Lock column option class lists in unit tests.
    Pin the full Bootstrap value set per breakpoint, including dividers, so a mapping change fails CI. Record that two-digit field suffixes are ignored.

--- Tests/Unit/Backend/ColumnOptionsProviderTest.php
+++ Tests/Unit/Backend/ColumnOptionsProviderTest.php
@@ -127,6 +127,71 @@ final class ColumnOptionsProviderTest extends TestCase
         );
     }
 
+    #[Test]
+    #[DataProvider('exactValueListProvider')]
+    public function breakpointExposesExactBootstrapClasses(string $field, array $expectedValues): void
+    {
+        $values = $this->values($this->subject->getColumnOptions($this->config($field)));
+
+        self::assertSame($expectedValues, $values);
+        self::assertContains('--div--', $values);
+        self::assertSame(2, count(array_keys($values, '--div--', true)));
+    }
+
+    public static function exactValueListProvider(): array
+    {
+        return [
+            'xs' => [
+                'xsCol1',
+                [
+                    'col', 'col-3', 'col-4', 'col-6', 'col-8', 'col-9', '--div--',
+                    'col-1', 'col-2', 'col-5', 'col-7', 'col-10', 'col-11', 'col-12', '--div--',
+                    '---', 'col-auto',
+                ],
+            ],
+            'sm' => [
+                'smCol1',
+                [
+                    ' ', 'col-sm-3', 'col-sm-4', 'col-sm-6', 'col-sm-8', 'col-sm-9', '--div--',
+                    'col-sm-1', 'col-sm-2', 'col-sm-5', 'col-sm-7', 'col-sm-10', 'col-sm-11', 'col-sm-12', '--div--',
+                    'col-sm-auto', 'd-sm-none',
+                ],
+            ],
+            'md' => [
+                'mdCol1',
+                [
+                    ' ', 'col-md-3', 'col-md-4', 'col-md-6', 'col-md-8', 'col-md-9', '--div--',
+                    'col-md-1', 'col-md-2', 'col-md-5', 'col-md-7', 'col-md-10', 'col-md-11', 'col-md-12', '--div--',
+                    'col-md-auto', 'd-md-none',
+                ],
+            ],
+            'lg' => [
+                'lgCol1',
+                [
+                    ' ', 'col-lg-3', 'col-lg-4', 'col-lg-6', 'col-lg-8', 'col-lg-9', '--div--',
+                    'col-lg-1', 'col-lg-2', 'col-lg-5', 'col-lg-7', 'col-lg-10', 'col-lg-11', 'col-lg-12', '--div--',
+                    'col-lg-auto', 'd-lg-none',
+                ],
+            ],
+            'xl' => [
+                'xlCol1',
+                [
+                    ' ', 'col-xl-3', 'col-xl-4', 'col-xl-6', 'col-xl-8', 'col-xl-9', '--div--',
+                    'col-xl-1', 'col-xl-2', 'col-xl-5', 'col-xl-7', 'col-xl-10', 'col-xl-11', 'col-xl-12', '--div--',
+                    'col-xl-auto', 'd-xl-none',
+                ],
+            ],
+        ];
+    }
+
+    #[Test]
+    public function twoDigitColumnIndexIsNotTreatedAsBreakpoint(): void
+    {
+        $result = $this->subject->getColumnOptions($this->config('xsCol10'));
+
+        self::assertSame([], $result['items']);
+    }
+
     private function config(string $field, array $items = []): array
     {
         return [
