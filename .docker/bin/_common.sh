e34e098e2ab600a9b71a39efc848fc0c838d02d6
Charles Coleman <charles@rapidfyre.com>
2026-08-12 22:00:07 -0600

    Rename Docker env template from .env.dist to .env.example.
    .env.example is the more common copy-this-to-.env name.

--- .docker/bin/_common.sh
+++ .docker/bin/_common.sh
@@ -4,8 +4,8 @@ set -e
 
 ensure_docker_env_file() {
     if [[ ! -f ".docker/.env" ]]; then
-        echo "Creating file '.docker/.env' since it didn't exist: cp .docker/.env.dist .docker/.env"
-        cp .docker/.env.dist .docker/.env
+        echo "Creating file '.docker/.env' since it didn't exist: cp .docker/.env.example .docker/.env"
+        cp .docker/.env.example .docker/.env
     fi
 }
 
